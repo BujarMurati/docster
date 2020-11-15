7187d69bbf2a4b2562546abb99eb970ed94b6035
BujarMurati <39311213+BujarMurati@users.noreply.github.com>
2020-11-16 00:57:37 +0100

    add content to documentation (#16)

--- docster/main.py
+++ docster/main.py
@@ -10,6 +10,16 @@ from .models import Package, OutputMode
 from .visitor import parse
 from .helpers import find_modules, resolve_module_name
 
+mode_help = dedent(
+    """
+    Output mode, one of "file" | "directory" | "stdout".
+    For "file" and "stdout" the template receives a Package object and is written to
+    "output" or echoed standardoutput. For "directory", the template receives one
+    Module object per module and is written to one file per module, with the output
+    directory structure mimicking the package structure. Defaults to "stdout"
+    """
+)
+
 
 def process_package(package_root: Path) -> Package:
     if not package_root.exists():
@@ -29,8 +39,6 @@ app = typer.Typer(
     help=dedent(
         """
     Extract docstrings from a python package and render them in a custom template.
-    For each python module, one file is rendered and an instance of docster.models.Module is
-    passed to the template.
     Docstring extraction is performed statically, meaning that your code is not being imported
     or run, so there is no need to worry about side effects.
     """
@@ -40,26 +48,26 @@ app = typer.Typer(
 
 @app.command()
 def remote(
-    uri: str = typer.Argument(...),
-    template_file: Path = typer.Option(Path("template.md"), "--template-file", "-t"),
-    output: Path = typer.Option(Path("build"), "--output", "-o"),
-    package_root: Path = typer.Option(Path("src"), "--package-root", "-p"),
-    mode: OutputMode = typer.Option(OutputMode.stdout.value, "--mode", "-m"),
+    uri: str = typer.Argument(
+        ..., help="a git uri from which to fetch a remote git repo"
+    ),
+    template_file: Path = typer.Option(
+        Path("template.md"), "--template-file", "-t", help="path to a Jinja2 template"
+    ),
+    output: Path = typer.Option(
+        Path("build"), "--output", "-o", help="where to write the output to"
+    ),
+    package_root: Path = typer.Option(
+        Path("src"),
+        "--package-root",
+        "-p",
+        help="the relative path to the package root inside the repo",
+    ),
+    mode: OutputMode = typer.Option(
+        OutputMode.stdout.value, "--mode", "-m", help=mode_help
+    ),
 ):
-    """Fetch a remote git repo, extract docstrings and render a template
-
-    Args:
-        uri (str): a uri from which to fetch a remote git repo
-        template_path (Path): path to a template
-        out_dir (Path): where to write the output to
-        package_root (Path): the relative path to the package root inside the repo
-        mode: (OutputMode, optional): Output mode, one of "file" | "directory" | "stdout".
-            For "file" and "stdout" the template receives a Package object and is written to
-            "output" or echoed standardoutput. For "directory", the template receives one
-            Module object per module and is written to one file per module, with the output
-            directory structure mimicking the package structure. Defaults to "stdout"
-    """
-
+    """Fetch a remote git repo, extract docstrings and render a template"""
     with TemporaryDirectory() as tmp_dir:
         directory = Path(Repo.clone_from(uri, tmp_dir).working_dir) / package_root
         package = process_package(directory)
@@ -68,24 +76,18 @@ def remote(
 
 @app.command()
 def local(
-    directory: Path = typer.Argument(Path(".")),
-    template_file: Path = typer.Option(Path("template.md"), "--template-file", "-t"),
-    output: Path = typer.Option(Path("build"), "--output", "-o"),
-    mode: OutputMode = typer.Option(OutputMode.stdout.value, "--mode", "-m"),
+    directory: Path = typer.Argument(Path("."), help="path to the package"),
+    template_file: Path = typer.Option(
+        Path("template.md"), "--template-file", "-t", help="path to a Jinja2 template"
+    ),
+    output: Path = typer.Option(
+        Path("build"), "--output", "-o", help="where to write the output to"
+    ),
+    mode: OutputMode = typer.Option(
+        OutputMode.stdout.value, "--mode", "-m", help=mode_help
+    ),
 ):
-    """Extract docstrings from a local package and render a template
-
-    Args:
-        dir (Path, optional): path to the package. Defaults to ".".
-        template_path (Path, optional): jinja2 template. Defaults to "template.md".
-        output (Path, optional): where to write rendered templates. Defaults to "/build".
-        mode: (OutputMode, optional): Output mode, one of "file" | "directory" | "stdout".
-            For "file" and "stdout" the template receives a Package object and is written to
-            "output" or echoed standardoutput. For "directory", the template receives one
-            Module object per module and is written to one file per module, with the output
-            directory structure mimicking the package structure. Defaults to "stdout"
-    """
-
+    """Extract docstrings from a local package and render a template"""
     package = process_package(directory)
     mode.apply(package, output, template_file)
 
